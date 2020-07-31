aacbb09181caf181a301947c737f6e35bed7a9a7
Andrew Peterson <normalandrew@gmail.com>
2020-07-31 09:13:13 -0700

    Added Navbar Burger Menu functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 
 const Navbar = ({ siteTitle }) => {
+  const [isActive, setIsActive] = useState(false)
+
   return (
-    <nav
-      className="navbar is-transparent"
-      role="navigation"
-      aria-label="main-navigation"
-    >
-      <div className="container">
+    <div className="container">
+      <nav
+        className="navbar is-transparent"
+        role="navigation"
+        aria-label="main-navigation"
+      >
         <div className="navbar-brand">
           <Link to="/" className="navbar-item" title="Logo">
             {/* <img src={logo} alt="Kaldi" style={{ width: '88px' }} /> */}
             <h4 className="is-size-4">{siteTitle}</h4>
           </Link>
           {/* Hamburger menu */}
-          <div className="navbar-burger burger" data-target="navMenu">
+          <a
+            onClick={() => setIsActive(!isActive)}
+            role="button"
+            className={`navbar-burger burger ${isActive ? 'is-active' : ''}`}
+            data-target="navMenu"
+            aria-label="menu"
+            aria-expanded="false"
+          >
             <span />
             <span />
             <span />
-          </div>
+          </a>
         </div>
-        <div id="navMenu" className="navbar-menu">
+        <div
+          id="navMenu"
+          className={`navbar-menu ${isActive ? 'is-active' : ''}`}
+        >
           <div className="navbar-start has-text-centered">
             <Link className="navbar-item" to="/about">
               About
@@ -37,8 +49,8 @@ const Navbar = ({ siteTitle }) => {
               </Link>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   )
 }
 
