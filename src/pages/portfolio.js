521f79147cbf95c6b72b3994a12d0334589d0d68
Andrew Peterson <normalandrew@gmail.com>
2020-08-03 06:54:37 -0700

    Remove useState that was not being used

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useStaticQuery, graphql, Link } from 'gatsby'
 import Img from 'gatsby-image'
 import Layout from '../components/Layout'
