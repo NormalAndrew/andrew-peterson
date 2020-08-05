3eeef82fba4d9957e182bd3346b28f885319c632
Andrew Peterson <normalandrew@gmail.com>
2020-08-05 06:22:49 -0700

    Added remark videos ability

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,28 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-embed-video`,
+            options: {
+              width: 800,
+              ratio: 1.77, // Optional, Defaults to 16/9 = 1.77
+              related: false,  // Optional, Will remove related videos from the end of an embedded Youtube video
+              noIframeBorder: true, // Optional: Disable insertion of <style> border: 0
+              urlOverrides: [
+                {
+                  id: 'youtube',
+                  embedURL: (videoId) => `https://www.youtube-nocookie.com/embed/${videoId}`,
+                }
+              ] // Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
+            },
+          },
+        ]
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
